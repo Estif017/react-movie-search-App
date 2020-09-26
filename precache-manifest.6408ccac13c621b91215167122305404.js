2b350607a030afd3b9ec007572eab844d4963408
Estif017 <estifoteklearegay@gmail.com>
2020-09-26 14:27:06 -0700

    Updates

---
+++ precache-manifest.6408ccac13c621b91215167122305404.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b15268308ee17d7cd3501367dfd35ba",
+    "url": "/react-movie-search-App/index.html"
+  },
+  {
+    "revision": "2101bc329e1c7d43e5ac",
+    "url": "/react-movie-search-App/static/css/main.5a3b0f1c.chunk.css"
+  },
+  {
+    "revision": "b0ccca698393121345b5",
+    "url": "/react-movie-search-App/static/js/2.0014c668.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-movie-search-App/static/js/2.0014c668.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2101bc329e1c7d43e5ac",
+    "url": "/react-movie-search-App/static/js/main.38a15f12.chunk.js"
+  },
+  {
+    "revision": "dcc0e67bcff2136fa982",
+    "url": "/react-movie-search-App/static/js/runtime-main.691ef3db.js"
+  }
+]);
